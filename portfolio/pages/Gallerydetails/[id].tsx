6ad6ac68fd8df76150e7df2d09e24792d9bfde19
MisanToghanro <tmisan657@gmail.com>
2026-01-02 12:40:34 +0100

    modified Gallerydetails

--- portfolio/pages/Gallerydetails/[id].tsx
+++ portfolio/pages/Gallerydetails/[id].tsx
@@ -21,12 +21,10 @@ const GalleryDetailPage = () => {
   <div className="min-h-screen">
      <Link href={`/Projects`}
       className="font-medium text-md shadow-md rounded-md px-4 py-2  hover:scale-105"> 
-      <span>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
 </svg>
 
-      </span>
       Back to gallery
       </Link>
 <GalleryDetail project={project} />;
