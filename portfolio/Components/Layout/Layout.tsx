9bd16c95ebeba17e07e0b8bf05a65993823a3ef9
MisanToghanro <tmisan657@gmail.com>
2026-01-03 14:37:42 +0100

    feat: add global site title and meta description v ia layout

--- portfolio/Components/Layout/Layout.tsx
+++ portfolio/Components/Layout/Layout.tsx
@@ -1,21 +1,28 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import Head from "next/head";
+import React, { ReactNode } from "react";
 
-import React, { Children, ReactNode } from "react";
-
-interface layoutProps {
-    children: ReactNode
+interface LayoutProps {
+  children: ReactNode;
 }
-const Layout: React.FC<layoutProps> = ({children}) => {
 
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <title>Wizzy Graphics | Creative Visual Designer</title>
+        <meta
+          name="description"
+          content="Wizzy Graphics – Professional graphic design, branding, flyers, and visual storytelling."
+        />
+      </Head>
 
-    return(
-<>
-<Header/>
-<main className="min-h-screen flex flex-col">{children}</main>
-<Footer/>
-</>
-    )
-}
+      <Header />
+      <main className="min-h-screen flex flex-col">{children}</main>
+      <Footer />
+    </>
+  );
+};
 
 export default Layout;
